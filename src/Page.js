92054cc4946bdee064a564ee079be283523684e6
danielromero27 <danielromero@mail.weber.edu>
2017-04-03 00:45:18 -0600

    Text, image, and icon changes.

--- src/Page.js
+++ src/Page.js
@@ -10,16 +10,16 @@ class Page extends Component {
 
             <header className="header">
                 <div className="constrain">
-                <h1>LOGO</h1>
+                <img src="https://cdn2.iconfinder.com/data/icons/the-mix-of-electronic-things/64/electro_synthesizer-128.png" alt=""/>    
 
                 <nav className="navigation">
                     <a className="nav-toggle" href="#">
                     <span className="trigger">&#9776;</span>
                     <ul>
                         <li>Home</li>
-                        <li>Contact Us</li>
-                        <li>Prices</li>
-                        <li>Cart</li>
+                        <li>Current Synths</li>
+                        <li>Wish List</li>
+                        <li>About</li>
                     </ul>
                     </a>
 
@@ -30,11 +30,12 @@ class Page extends Component {
             <section className="main">
                 <div className="constrain">
                 <article className="primary">
-                    <h1>Article Title</h1>
-                    <img src="http://lorempixel.com/400/200/" alt="" className="hero"/>
+                    <img src="http://3.bp.blogspot.com/-uW31Zv-iQuE/UC3dTGb7M-I/AAAAAAAFDzM/-LUpLVGxKzM/s1600/Mantle%2BKorg%2BMagnasonic.jpg" alt="" className="hero"/>
+                   
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda excepturi, incidunt quae optio, accusamus, perferendis ducimus a fugiat molestias laboriosam provident odio architecto officia quos nesciunt. Eveniet nesciunt doloremque facilis.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, doloremque. Quaerat, id minima assumenda ad qui possimus debitis harum, soluta quia ipsum, atque sed adipisci est accusantium cumque quasi! Assumenda.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, doloremque. Quaerat, id minima assumenda ad qui possimus debitis harum, soluta quia ipsum, atque sed adipisci est accusantium cumque quasi! Assumenda.</p>
+                    
                 </article>
                 <aside className="secondary"></aside>
                 </div>
@@ -42,9 +43,11 @@ class Page extends Component {
 
             <footer className="footer">
                 <div className="constrain">
-                <p>Copyright 2017</p>
-                <p>Legal Stuff</p>
-                <p>Careers</p>
+                    <ul>
+                        <li>Copyright 2017</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
                 </div>
             </footer>
 
